88d04dd5cb6b26f33ff7a460f309320fed360b12
Damian Gręda <damian.greda@westwing.de>
2016-03-13 15:18:49 +0100

    Added FTP to the factory

---
+++ Examples/example4.php
@@ -0,0 +1,14 @@
+<?php
+
+// Providing a Yaml config file to the Factory
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Westwing\Filesystem\Factory;
+
+$fileSystemFactory = new Factory();
+$fileSystemFactory->setConfigFile(__DIR__ . '/filesystem.yml');
+
+$fileSystem = $fileSystemFactory->get('ftpFS');
+
+print_r($fileSystem->listContents());
